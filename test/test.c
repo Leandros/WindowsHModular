ad6ae08c66bbfd0e4a09926daa1f02dc49259568
Arvid Gerstmann <dev@arvid-g.de>
2017-08-19 23:35:01 +0200

    Remove accidentally included object files

--- test/test.c
+++ test/test.c
@@ -1,27 +1,27 @@
 #if defined(TEST_MODULAR)
-#include <win32/windows_modular.h>
+    #include <win32/windows_modular.h>
 #elif defined(TEST_ATOMIC)
-#include <win32/atomic.h>
+    #include <win32/atomic.h>
 #elif defined(TEST_DBGHELP)
-#include <win32/dbghelp.h>
+    #include <win32/dbghelp.h>
 #elif defined(TEST_DDS)
-#include <win32/dds.h>
+    #include <win32/dds.h>
 #elif defined(TEST_FILE)
-#include <win32/file.h>
+    #include <win32/file.h>
 #elif defined(TEST_GDI)
-#include <win32/gdi.h>
+    #include <win32/gdi.h>
 #elif defined(TEST_IO)
-#include <win32/io.h>
+    #include <win32/io.h>
 #elif defined(TEST_MISC)
-#include <win32/misc.h>
+    #include <win32/misc.h>
 #elif defined(TEST_PROCESS)
-#include <win32/process.h>
+    #include <win32/process.h>
 #elif defined(TEST_SYSINFO)
-#include <win32/sysinfo.h>
+    #include <win32/sysinfo.h>
 #elif defined(TEST_THREADS)
-#include <win32/threads.h>
+    #include <win32/threads.h>
 #elif defined(TEST_WINDOW)
-#include <win32/window.h>
+    #include <win32/window.h>
 #endif
 
 
