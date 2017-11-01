311e5bb5321714243848659edeaaa51e238829f2
Arvid Gerstmann <dev@arvid-g.de>
2017-11-01 19:31:17 +0100

    Minor formatting improvements

--- include/win32/windows_base.h
+++ include/win32/windows_base.h
@@ -134,26 +134,28 @@
 /* ========================================================================== */
 
 #if defined(_MSC_VER)
-#if _MSC_VER < 1300
-typedef signed char int8_t;
-typedef unsigned char uint8_t;
-typedef signed short int16_t;
-typedef unsigned short uint16_t;
-typedef signed int int32_t;
-typedef unsigned int uint32_t;
+    /* 1300 == VC 6.0 */
+    #if _MSC_VER < 1300
+        typedef signed char         int8_t;
+        typedef unsigned char       uint8_t;
+        typedef signed short        int16_t;
+        typedef unsigned short      uint16_t;
+        typedef signed int          int32_t;
+        typedef unsigned int        uint32_t;
+    #else
+        typedef signed __int8       int8_t;
+        typedef unsigned __int8     uint8_t;
+        typedef signed __int16      int16_t;
+        typedef unsigned __int16    uint16_t;
+        typedef signed __int32      int32_t;
+        typedef unsigned __int32    uint32_t;
+    #endif
+    typedef signed __int64          int64_t;
+    typedef unsigned __int64        uint64_t;
 #else
-typedef signed __int8 int8_t;
-typedef unsigned __int8 uint8_t;
-typedef signed __int16 int16_t;
-typedef unsigned __int16 uint16_t;
-typedef signed __int32 int32_t;
-typedef unsigned __int32 uint32_t;
+    #include <stdint.h>
 #endif
-typedef signed __int64 int64_t;
-typedef unsigned __int64 uint64_t;
-#else
-#include <stdint.h>
-#endif
+
 
 typedef int                 BOOL;
 typedef char                CHAR;
