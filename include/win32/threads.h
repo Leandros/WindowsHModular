853ee8f8373ab4453df175b36fe479cd237d2888
Arvid Gerstmann <dev@arvid-g.de>
2018-04-17 18:07:18 +0200

    Add SRWLock functionality

--- include/win32/threads.h
+++ include/win32/threads.h
@@ -129,6 +129,11 @@ typedef struct _RTL_CONDITION_VARIABLE {
 typedef RTL_CONDITION_VARIABLE  CONDITION_VARIABLE;
 typedef PRTL_CONDITION_VARIABLE PCONDITION_VARIABLE;
 
+#define SRWLOCK_INIT {0}
+typedef struct _RTL_SRWLOCK {
+        PVOID Ptr;
+} RTL_SRWLOCK, *PRTL_SRWLOCK;
+typedef RTL_SRWLOCK SRWLOCK, *PSRWLOCK;
 
 /* ========================================================================== */
 /* Waiting Functions: */
@@ -244,6 +249,26 @@ LPVOID WINAPI TlsGetValue(
         DWORD   dwTlsIndex);
 DWORD WINAPI TlsFree(
         DWORD   dwTlsIndex);
+
+/* ========================================================================== */
+/* SRW Lock Functions: */
+void WINAPI InitializeSRWLock(PSRWLOCK SRWLock);
+
+void WINAPI AcquireSRWLockExclusive(PSRWLOCK SRWLock);
+void WINAPI AcquireSRWLockShared(PSRWLOCK SRWLock);
+
+void WINAPI ReleaseSRWLockExclusive(PSRWLOCK SRWLock);
+void WINAPI ReleaseSRWLockShared(PSRWLOCK SRWLock);
+
+BOOLEAN WINAPI TryAcquireSRWLockExclusive(PSRWLOCK SRWLock);
+BOOLEAN WINAPI TryAcquireSRWLockShared(PSRWLOCK SRWLock);
+
+BOOL WINAPI SleepConditionVariableSRW(
+    PCONDITION_VARIABLE ConditionVariable,
+    PSRWLOCK            SRWLock,
+    DWORD               dwMilliseconds,
+    ULONG               Flags);
+
 
 #if defined(__cplusplus)
 }
