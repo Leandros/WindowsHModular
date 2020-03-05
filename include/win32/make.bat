f090b825fd1cdca38c10e4752deff94f8e1b2a3d
Merlyn Morgan-Graham <kavika@gmail.com>
2020-03-05 00:08:38 -0800

    Fix extra whitespace in concat header gen script

--- include/win32/make.bat
+++ include/win32/make.bat
@@ -4,49 +4,48 @@
 del /F /Q windows.h
 
 :: Generate skeleton
-echo: > windows.h
-echo #ifndef _WINDOWS_ >> windows.h
-echo #pragma once >> windows.h
-echo #ifndef _INC_WINDOWS >> windows.h
-echo #define _INC_WINDOWS >> windows.h
-echo #ifndef WINDOWS_H >> windows.h
-echo #define WINDOWS_H >> windows.h
-echo: >> windows.h
+echo #ifndef _WINDOWS_>> windows.h
+echo #pragma once>> windows.h
+echo #ifndef _INC_WINDOWS>> windows.h
+echo #define _INC_WINDOWS>> windows.h
+echo #ifndef WINDOWS_H>> windows.h
+echo #define WINDOWS_H>> windows.h
+echo:>> windows.h
 
 :: Append files
-echo: >> windows.h
+echo:>> windows.h
 type windows_base.h >> windows.h
-echo: >> windows.h
+echo:>> windows.h
 type atomic.h >> windows.h
-echo: >> windows.h
+echo:>> windows.h
 type dbghelp.h >> windows.h
-echo: >> windows.h
+echo:>> windows.h
 type dds.h >> windows.h
-echo: >> windows.h
+echo:>> windows.h
 type file.h >> windows.h
-echo: >> windows.h
+echo:>> windows.h
 type gdi.h >> windows.h
-echo: >> windows.h
+echo:>> windows.h
 type io.h >> windows.h
-echo: >> windows.h
+echo:>> windows.h
 type misc.h >> windows.h
-echo: >> windows.h
+echo:>> windows.h
 type process.h >> windows.h
-echo: >> windows.h
+echo:>> windows.h
 type sysinfo.h >> windows.h
-echo: >> windows.h
+echo:>> windows.h
 type threads.h >> windows.h
-echo: >> windows.h
+echo:>> windows.h
 type window.h >> windows.h
-echo: >> windows.h
+echo:>> windows.h
 type intrin.h >> windows.h
-echo: >> windows.h
+echo:>> windows.h
 type fiber.h >> windows.h
-echo: >> windows.h
+echo:>> windows.h
 
 :: Finish skeleton
-echo #endif >> windows.h
-echo #endif >> windows.h
-echo #define _WINDOWS_ >> windows.h
-echo #endif >> windows.h
+echo #endif>> windows.h
+echo #endif>> windows.h
+echo #define _WINDOWS_>> windows.h
+echo #endif>> windows.h
 
