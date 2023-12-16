f6c61cb256eceea84f50db16be0e0732e6fa95fc
Jeremy <jeremy.laumon@gmail.com>
2023-12-16 21:09:55 +0100

    Added some USN Journal related types and functions

--- include/win32/windows_base.h
+++ include/win32/windows_base.h
@@ -379,6 +379,13 @@ typedef struct _FILETIME {
     DWORD dwHighDateTime;
 } FILETIME, *PFILETIME, *LPFILETIME;
 
+/* GUID: */
+typedef struct _GUID {
+    unsigned long  Data1;
+    unsigned short Data2;
+    unsigned short Data3;
+    unsigned char  Data4[8];
+} GUID;
 
 #if defined(__cplusplus)
 }
